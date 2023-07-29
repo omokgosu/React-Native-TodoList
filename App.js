04f354b4ee4a8c182630f54ed425053c612e8a8d
양진성 <anfakt0606@naver.com>
2023-07-29 16:41:18 +0900

    touchableopacity , textinput

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity,
+  TextInput } from 'react-native';
+import {theme} from './color'
+import { useState } from 'react';
+
 
 export default function App() {
+
+  const [working, setWorking] = useState(true);
+  const [text, setText]= useState('');
+  const trabel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto'/>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.gray}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={trabel}>
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.gray}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+        <TextInput 
+          onChangeText={onChangeText}
+          placeholder={working ? "Add a To Do" : "where do you want a go?"}
+          style={styles.input}/>
     </View>
   );
 }
@@ -13,8 +39,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
+  header:{
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100
+  },
+  btnText: {
+    fontSize: 44,
+    fontWeight: '600'
+  },
+  input:{
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18
+  }
 });
